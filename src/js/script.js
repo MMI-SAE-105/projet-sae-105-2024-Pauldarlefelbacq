06ee6ea0f6700a8352af57f8a05e736f6b173338
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2025-01-03 18:22:27 +0100

    css articles

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,20 @@ if (toggle && nav) {
     page.classList.toggle("noscroll", isClosed);
   });
 }
+
+/* changement langue*/
+const lang = document.getElementsByTagName("HTML")[0].getAttribute("lang");
+const barrefr = document.querySelector(".batonfr");
+const barreen = document.querySelector(".batonen");
+const langtouch = document.querySelector(".header__lang");
+
+langtouch.addEventListener("click", () => {
+  if (lang == "fr"){
+    barrefr.ariaHidden = "true";
+    barreen.ariaHidden = "false";
+  }
+  else {
+    barrefr.ariaHidden ="false";
+    barreen.ariaHidden = "true";
+  }
+});

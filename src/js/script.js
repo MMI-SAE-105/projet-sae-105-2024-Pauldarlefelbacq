5857fc43fc9bde525cf25cc4aa73499c6598d935
Pauldarlefelbacq <paul.darle-felbacq@edu.univ-fcomte.fr>
2025-01-07 16:23:54 +0100

    jscript changement langue/ carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+/*menu*/
+
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".header__menu");
 const page = document.body;
@@ -13,18 +15,36 @@ if (toggle && nav) {
 }
 
 /* changement langue*/
+
 const lang = document.getElementsByTagName("HTML")[0].getAttribute("lang");
 const barrefr = document.querySelector(".batonfr");
 const barreen = document.querySelector(".batonen");
 const langtouch = document.querySelector(".header__lang");
 
-langtouch.addEventListener("click", () => {
-  if (lang == "fr"){
-    barrefr.ariaHidden = "true";
-    barreen.ariaHidden = "false";
-  }
-  else {
-    barrefr.ariaHidden ="false";
-    barreen.ariaHidden = "true";
-  }
-});
+if (lang == "fr"){
+  barrefr.ariaHidden = "false";
+  barreen.ariaHidden = "true";
+}
+else {
+  barrefr.ariaHidden ="true";
+  barreen.ariaHidden = "false";
+}
+
+
+/*carroussel*/
+
+const carousel = document.querySelector(".carroussel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+const premierItem = document.querySelector(".carroussel__item");
+const scrollAmount = premierItem.clientWidth;
+
+if (carousel){
+    prevButton.addEventListener("click", () =>{
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth"});
+    });
+    nextButton.addEventListener("click", () =>{
+        carousel.scrollBy({ left: scrollAmount, behavior: "smooth"});
+    });
+}
